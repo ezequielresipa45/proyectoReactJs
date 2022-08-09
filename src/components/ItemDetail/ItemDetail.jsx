ec01b1a720e9fdaf2a9feb47f2091200194865c3
Ezequiel <ezequielresipa45@gmail.com>
2022-08-09 11:21:01 -0300

    Agregado de ItemCount en componente ItemDetails

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
 import '../ItemDetail/ItemDetail.css'
+import ItemCount from "../ItemCount/ItemCount";
+import React, { useState } from "react";
 
-export default function ItemDetail({ price, images, modelo, marca, detalles }) {
+export default function ItemDetail({ price, images, modelo, marca, detalles, addCard, cant }) {
+
+  const [count, setCount] = useState(1);
 
   return (
     <div className='item-detail'>
@@ -12,14 +15,13 @@ export default function ItemDetail({ price, images, modelo, marca, detalles }) {
           <p>{detalles}</p>
           <p>Cuando se trata de manejar tu {marca} {modelo} no hay tiempo que perder. Su sistema de desbloqueo y arranque automático que detecta las llaves por proximidad hacen todo mucho más simple.</p>
           <p>El interior del {marca} {modelo} fue diseñado especialmente para que disfrutes el espacio amplio y para que accedas fácilmente a todos los accesorios.</p>
-          <p>Adquieralo solo por <span>{price}</span></p>
+          <div className="group-price-count">
+            <p>Adquieralo solo por <span>{price}</span></p>
+            <ItemCount cantidad={cant} count={count} setCount={setCount} />
+            <button onClick={() => addCard(marca, count)}>Añadir al Carrito</button>
+          </div>
         </div>
       </div>
-
-
-
-
-      
     </div>
   )
 }
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -3,10 +3,22 @@ import ItemDetail from '../ItemDetail/ItemDetail'
 import ItemsData from '../../data/data'
 import '../ItemDetailContainer/ItemDetailContainer.css'
 import Spinner from '../Spinner/Spinner'
-
+import Swal from "sweetalert2";
 
 
 export default function ItemDetailContainer() {
+
+
+    const addCard = (producto, cantidad) => {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `Has añadido ${cantidad} ${producto} al carrito`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      };
+
 
     // PROMISE 
     function getDetail() {
@@ -31,13 +43,14 @@ export default function ItemDetailContainer() {
             <div className='ItemDetailContainer'>
                 <h1>Detalles del {data.car_make}</h1>
                 <ItemDetail
+                    addCard={addCard}
                     marca={data.car_make}
                     detalles={data.detalles}
                     images={data.img}
                     modelo={data.car_model}
                     key={data.id}
                     price={data.price}
-
+                    cant={5}
                 />
             </div>
 
