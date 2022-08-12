c1100c399da3930b8b4229c9bd9f3bbb593b2784
Ezequiel <ezequielresipa45@gmail.com>
2022-08-11 23:09:03 -0300

    Correccion de bugs

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,46 +1,36 @@
 import "./ItemListContainer.css";
-import React, { useEffect, useState } from 'react'
-import ItemsData from '../../data/data'
-import ItemList from '../ItemList/ItemList'
-import { useParams}  from "react-router-dom";
-
-
-
+import React, { useEffect, useState } from "react";
+import ItemsData from "../../data/data";
+import ItemList from "../ItemList/ItemList";
+import { useParams } from "react-router-dom";
 
 export default function ItemListContainer({ greeting }) {
-
-  // PROMISE 
+  // PROMISE
   function getProductos() {
     return new Promise((resolve) => {
-      setTimeout(() => resolve(ItemsData), 2000)
-    })
-  };
+      setTimeout(() => resolve(ItemsData), 2000);
+    });
+  }
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-
-const Categoria =  useParams().idCategorias;
+  const Categoria = useParams().idCategorias;
 
   useEffect(() => {
-    getProductos().then((respuesta) => {
-      let filtrados =  respuesta.filter(elemento => elemento.car_make == Categoria );
-
-
-      Categoria === undefined ?  setData(respuesta) : 
-
-     setData(filtrados);
-
-    }).catch((e) => e)
-  }, [])
-
+    if (Categoria) {
+      getProductos().then(
+        resolve => setData(resolve.filter((e) => e.car_make == Categoria))
+      );
+    } else {
+      getProductos().then((resolve) => setData(resolve));
+    }
+  })
 
   return (
     <div className="ItemListContainer">
       <h2 className="greeting">{greeting}</h2>
       <div className="containerProducts">
-        <ItemList
-          data={data} />
-       
+        <ItemList data={data} />
       </div>
     </div>
   );
